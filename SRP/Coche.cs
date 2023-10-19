1d68eabbe6fca582d0c0e29dd136656ec52bb17b
AKTIOS\santiago.martinez <santiago.martinezz@aktios.com>
2023-10-19 13:05:54 +0200

    added srp

---
+++ SRP/Coche.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.SRP
+{
+    public class Coche
+    {
+        private int Id {  get; set; }
+        private string Marca { get; set; }
+        private string Modelo {  get; set; }
+        private int Anio {  get; set; }
+
+        public Coche(int id, string marca, string modelo, int anio)
+        {
+            Id = id;
+            Marca = marca;
+            Modelo = modelo;
+            Anio = anio;
+        }
+    }
+}
